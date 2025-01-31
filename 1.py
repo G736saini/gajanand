59cf659379debf9ad18399af92287649c5825874
G736saini <sainigajanand003@gmail.com>
2025-01-31 16:25:04 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,21 @@
+num1 = int(input())
+num2 = int(input())
+respons = input("add,sub,mul,divid,pow")
+if(respons == "add"):
+    num3 = num1 + num2
+    print(num3)
+elif(respons == "sub"):
+    num3  = num2 - num1
+    print(num3)
+elif(respons == "sub"):
+    num3  = num2 - num1
+    print(num3)
+elif(respons == "mul"):
+    num3  = num2 * num1
+    print(num3)
+elif(respons == "divid"):
+    num3  = num2 // num1
+    print(num3)
+elif(respons == "pow"):
+    num3  = num2 ** num1
+    print(num3)
